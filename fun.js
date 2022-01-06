40fbf20cfec4d5019b97a2d3b22c1e0fcfafda81
ahmed <71600486+Ahmed-Aoulad-Amar@users.noreply.github.com>
2022-01-06 02:18:55 +0100

    Update fun.js

--- fun.js
+++ fun.js
@@ -1,4 +1,4 @@
-const cityName = document.getElementById("cityName"); 
+const cityName = document.getElementById("cityName");
 const country = document.getElementById("country");
 const timeZone = document.getElementById("timeZone");
 const coords = document.querySelector(".coord").querySelectorAll("p");
@@ -14,58 +14,66 @@ const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 function getData(location) {
-    let loc = location;
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=48ddfe8c9cf29f95b7d0e54d6e171008`;
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.onload = function () {
-        if (this.status === 200) {
-            let data = JSON.parse(this.responseText);
-            cityName.textContent = `Location : ${data.name}`;
-            country.textContent = `Country : ${data.sys.country}`;
-            timeZone.textContent = `Timezone : ${data.timezone}`;
-            coords[0].textContent = `Longitude : ${data.coord.lon}`;
-            coords[1].textContent = `Latitude : ${data.coord.lat}`;
-            weatherType.textContent = `Weather type : ${data.weather[0].main}`;
-            weatherDesc.textContent = `Weather description : ${data.weather[0].description}`;
-            weatherImg.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-            temp[0].textContent = `Original Temperature : ${ktc(data.main.temp)}`;
-            temp[1].textContent = `But it feels like ${ktc(data.main.feels_like)}`;
-            temp[2].textContent = `Min temperature ${ktc(data.main.temp_min)}`;
-            temp[3].textContent = `Max temperature ${ktc(data.main.temp_max)}`;
-            temp[4].textContent = `Pressure : ${data.main.pressure}`;
-            temp[5].textContent = `Humidity : ${data.main.humidity}`;
-            visibility.textContent = `Visibility : ${data.visibility}`;
-            wind[0].textContent = `Wind speed : ${data.wind.speed}`;
-            wind[1].textContent = `Wind deg : ${data.wind.deg}`;
-        } else {alert('Some error occured');}}
-        xhr.send();}
+  let loc = location;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=48ddfe8c9cf29f95b7d0e54d6e171008`;
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.onload = function () {
+    if (this.status === 200) {
+      let data = JSON.parse(this.responseText);
+      cityName.textContent = `Location : ${data.name}`;
+      country.textContent = `Country : ${data.sys.country}`;
+      timeZone.textContent = `Timezone : ${data.timezone}`;
+      coords[0].textContent = `Longitude : ${data.coord.lon}`;
+      coords[1].textContent = `Latitude : ${data.coord.lat}`;
+      weatherType.textContent = `Weather type : ${data.weather[0].main}`;
+      weatherDesc.textContent = `Weather description : ${data.weather[0].description}`;
+      weatherImg.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      temp[0].textContent = `Original Temperature : ${ktc(data.main.temp)}`;
+      temp[1].textContent = `But it feels like ${ktc(data.main.feels_like)}`;
+      temp[2].textContent = `Min temperature ${ktc(data.main.temp_min)}`;
+      temp[3].textContent = `Max temperature ${ktc(data.main.temp_max)}`;
+      temp[4].textContent = `Pressure : ${data.main.pressure}`;
+      temp[5].textContent = `Humidity : ${data.main.humidity}`;
+      visibility.textContent = `Visibility : ${data.visibility}`;
+      wind[0].textContent = `Wind speed : ${data.wind.speed}`;
+      wind[1].textContent = `Wind deg : ${data.wind.deg}`;
+    } else {
+      alert("Some error occured");
+    }
+  };
+  xhr.send();
+}
 function ktc(k) {
-    k = (k - 273.15);
-    return k.toFixed(2);}
+  k = k - 273.15;
+  return k.toFixed(2);
+}
 const searchBtn = document.querySelector(".searchBox").querySelector("button");
 const searchBox = document.querySelector(".searchBox").querySelector("input");
 searchBox.addEventListener("keypress", (e) => {
-    if (e.which === 13) {
-        let searchVal = searchBox.value;
-        if (searchVal === "") {
-            alert("Enter location name in the search box")
-        } else if (searchVal !== "") {
-            searchVal = searchVal.split("");
-            searchVal[0] = searchVal[0].toUpperCase();
-            searchVal = searchVal.join("");
-            getData(searchVal);}}})
-searchBtn.addEventListener("click", () => {
+  if (e.which === 13) {
     let searchVal = searchBox.value;
     if (searchVal === "") {
-        alert("Enter location name in the search box")
+      alert("Enter location name in the search box");
     } else if (searchVal !== "") {
-        searchVal = searchVal.split("");
-        searchVal[0] = searchVal[0].toUpperCase();
-        searchVal = searchVal.join("");
-        getData(searchVal);
+      searchVal = searchVal.split("");
+      searchVal[0] = searchVal[0].toUpperCase();
+      searchVal = searchVal.join("");
+      getData(searchVal);
     }
-})
+  }
+});
+searchBtn.addEventListener("click", () => {
+  let searchVal = searchBox.value;
+  if (searchVal === "") {
+    alert("Enter location name in the search box");
+  } else if (searchVal !== "") {
+    searchVal = searchVal.split("");
+    searchVal[0] = searchVal[0].toUpperCase();
+    searchVal = searchVal.join("");
+    getData(searchVal);
+  }
+});
 async function getUser(username) {
   try {
     const { data } = await axios(APIURL + username);
@@ -86,7 +94,7 @@ async function getProjict(username) {
   }
 }
 function createUserCard(user) {
-    const cardHTML = `
+  const cardHTML = `
                     <style>
                         .mmmm{margin-left: 35px;
                                margin-top: 50px;
@@ -131,8 +139,8 @@ function createErrorCard(msg) {
   main.innerHTML = cardHTML;
 }
 function addProjictToCard(Projict) {
-    const ProjictEl = document.getElementById("Projict");
-    Projict.slice(0, 5).forEach((INproject) => {
+  const ProjictEl = document.getElementById("Projict");
+  Projict.slice(0, 5).forEach((INproject) => {
     const projectLINK = document.createElement("a");
     projectLINK.classList.add("INproject");
     projectLINK.href = INproject.html_url;
@@ -142,11 +150,10 @@ function addProjictToCard(Projict) {
   });
 }
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const user = search.value;
+  e.preventDefault();
+  const user = search.value;
   if (user) {
     getUser(user);
     search.value = "";
   }
 });
-
